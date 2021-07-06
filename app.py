ee1fbeb8344fdd699ea421d221fdede746b7b320
RajeshThakur11 <rajeshthakur1r@gmail.com>
2021-07-06 20:06:55 +0530

    latest clone

--- app.py
+++ app.py
@@ -39,28 +39,28 @@ def api_response(request):
         error= {"error": "something went wrong!!! Try again"}
         return error
 
-@app.route("/",methods=["GET","POST"])
-def index():
-    if request.method=="POST":
-        try:
-            if request.form:
-                data = dict(request.form).values()
-                data = [list(map(float, data))]
-                response = predict(data)
-                return render_template("index.html", response=response)
+    @app.route("/",methods=["GET","POST"])
+    def index():
+        if request.method=="POST":
+            try:
+                if request.form:
+                    data = dict(request.form).values()
+                    data = [list(map(float, data))]
+                    response = predict(data)
+                    return render_template("index.html", response=response)
 
-            # If Request is coming from API
-            elif request.json:
-                response= api_response(request)
-                return jsonify(response)
+                # If Request is coming from API
+                elif request.json:
+                    response= api_response(request)
+                    return jsonify(response)
 
-        except Exception as e:
-            print(e)
-            error = {"error": "something went wrong!!! Try again"}
-            return error
+            except Exception as e:
+                print(e)
+                error = {"error": "something went wrong!!! Try again"}
+                return error
 
-    else:
-        return "RenderIndexTemplate"
+        else:
+            return "RenderIndexTemplate"
 
 
 
